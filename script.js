d3020cf6805546c3ee13c9e333c1208107d4c4e6
Meghna Datta <meghna.datta@calsoftinc.com>
2025-07-22 22:40:36 +0530

    update portfolio

--- script.js
+++ script.js
@@ -3,20 +3,43 @@ let navbar=document.querySelector('.navbar');
 let sections=document.querySelectorAll('section');
 let navlinks=document.querySelectorAll('header nav a');
 
+// window.onscroll = () => {
+//     sections.forEach(sec =>{
+//         let top=window.scrollY;
+//         let offset=sec.offsetTop-150;
+//         let height=sec.offsetHeight;
+//         let id=sec.getAttribute('id');
+//         if(top>=offset && top< offset+height){
+//             navlinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active')
+//             })
+//         }
+//     })
+// }
+
 window.onscroll = () => {
-    sections.forEach(sec =>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop-150;
-        let height=sec.offsetHeight;
-        let id=sec.getAttribute('id');
-        if(top>=offset && top< offset+height){
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active')
-            })
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            const activeLink = document.querySelector(`header nav a[href*="${id}"]`);
+            if (activeLink) {
+                activeLink.classList.add('active');
+            }
         }
-    })
-}
+    });
+};
+
+
 menuicon.onclick=()=>{
     menuicon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
